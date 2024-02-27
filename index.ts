ca3ab9a09f43554791bd28ebc5684956abaae450
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-02-27 21:42:55 +0000

    interface type

--- index.ts
+++ index.ts
@@ -42,3 +42,18 @@ let sing = (): void => {
 let error = (): never => {
     throw Error("ooops");
 };
+
+// Interface
+interface RobotArmy {
+    count: number,
+    type: string,
+    magic: string
+}
+
+let fightRobotArmy = (robots: RobotArmy) => {
+    console.log("FIGHT!!");
+}
+
+let fightRobotArmy2 = (robots: { count: number, type: string, magic: string }) => {
+    console.log("FIGHT!!");
+}
